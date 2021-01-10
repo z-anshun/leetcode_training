e42e678b43ea94d416155fedba9ad229ef734c5d
zhanganshun <1374658781@qq.com>
2021-01-10 23:41:32 +0800

    1-10

---
+++ leetcode/1486.go
@@ -0,0 +1,22 @@
+package leetcode
+
+/*
+ @Author: as
+ @Date: Creat in 23:30 2021/1/10
+ @Description: algorithm
+*/
+
+// 1486. 数组异或操作
+// 给你两个整数，n 和 start 。
+//
+// 数组 nums 定义为：nums[i] = start + 2*i（下标从 0 开始）且 n == nums.length 。
+//
+// 请返回 nums 中所有元素按位异或（XOR）后得到的结果。
+
+func xorOperation(n int, start int) int {
+	res:=0
+	for i:=0;i<n;i++{
+		res=(start+i*2)^res
+	}
+	return res
+}
